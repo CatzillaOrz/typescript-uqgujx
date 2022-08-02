ac2dd86058a96cec2a465bcd9be1031ce633e54a
CatzillaOrz <catzillaorz@outlook.com>
2022-08-02 16:20:25 +0800

    4 files modified, 1 added

--- index.ts
+++ index.ts
@@ -1,24 +1,26 @@
 import axios from 'axios';
 import './style.css';
+import GMap from './map';
 
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('adress')! as HTMLInputElement;
 
-const GOOGLE_API_KEY = 'AIzaSyCIaAc2c5M3VpbCH6PPq_ guwy9lHuowXOs';
+const GOOGLE_API_KEY = '422c2369d0bc45fb6bd97194626adf20';
 
 function searchAdressHandler(event: Event) {
   event.preventDefault();
-  const enteredAdress = addressInput.value;
+  // const enteredAdress = addressInput.value;
 
   // send to google API
-  axios
-    .get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-        enteredAdress
-      )}&key=${GOOGLE_API_KEY}`
-    )
-    .then((data) => console.log(data))
-    .catch((err) => console.log('error:', err));
+  // axios
+  //   .get(
+  //     `https://restapi.amap.com/v3/config/district?keywords=${enteredAdress}&subdistrict=2&key=${GOOGLE_API_KEY}&extensions=base`
+  //   )
+  //   .then((data) => console.log(data))
+  //   .catch((err) => console.log('error:', err));
 }
 
 form.addEventListener('submit', searchAdressHandler);
+
+const iMap = GMap.getInstance();
+iMap.initMap();
---
+++ map.ts
@@ -0,0 +1,43 @@
+import AMapLoader from '@amap/amap-jsapi-loader';
+
+export default class GMap {
+  readonly key = '422c2369d0bc45fb6bd97194626adf20';
+  private map;
+
+  private static instance: GMap;
+  districtSearch;
+  private constructor() {}
+
+  static getInstance() {
+    if (!GMap.instance) {
+      return new GMap();
+    }
+    return this.instance;
+  }
+
+  initMap() {
+    AMapLoader.load({
+      key: this.key, // 申请好的Web端开发者Key，首次调用 load 时必填
+      version: '2.0', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      AMapUI: {
+        // 是否加载 AMapUI，缺省不加载
+        version: '1.1', // AMapUI 版本
+        plugins: ['overlay/SimpleMarker'], // 需要加载的 AMapUI ui插件
+      },
+      Loca: {
+        // 是否加载 Loca， 缺省不加载
+        version: '2.0', // Loca 版本
+      },
+    })
+      .then((AMap) => {
+        this.map = new AMap.Map('container', {
+          zoom: 12,
+          center: [116.39, 39.9],
+        });
+      })
+      .catch((e) => {
+        console.error(e); //加载错误提示
+      });
+  }
+}
